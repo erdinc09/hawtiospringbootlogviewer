489b15003814a10b1396417a1e9b45e3ad1b40c7
Ahmet Erdinç Yılmaz <ahmeterdinc09@gmail.com>
2024-04-30 16:41:30 +0300

    initilize project structure

---
+++ logviewer/src/test/java/io/github/erdinc09/springboot/hawtio/logviewer/LogviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.erdinc09.springboot.hawtio.logviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ logviewertester/src/main/java/io/github/erdinc09/springboot/hawtio/logviewertester/LogViewerTesterApplication.java
@@ -0,0 +1,13 @@
+package io.github.erdinc09.springboot.hawtio.logviewertester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogViewerTesterApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogViewerTesterApplication.class, args);
+	}
+
+}
