afc3ed9b1ed7b6ac5fd93152d6a5d0ea13e9da1b
Ahmet Erdinç Yılmaz <ahmeterdinc09@gmail.com>
2024-05-01 13:05:51 +0300

    rename sample plugin

---
+++ logviewer/frontend/src/bootstrap.tsx
@@ -0,0 +1,22 @@
+import { hawtio, Hawtio, registerPlugins } from "@hawtio/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { plugin } from "./springbootlogviewer";
+
+// Register builtin plugins
+registerPlugins();
+
+// Register the plugin under development
+plugin();
+
+// Bootstrap Hawtio
+hawtio.bootstrap();
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Hawtio />
+  </React.StrictMode>
+);
---
+++ logviewer/frontend/src/springbootlogviewer/simple/globals.ts
@@ -0,0 +1,7 @@
+import { Logger } from '@hawtio/react'
+
+export const pluginName = 'springbootlogviewer'
+export const pluginTitle = 'Spring Boot Log Viewer'
+export const pluginPath = '/springbootlogviewer'
+
+export const log = Logger.get(pluginName) 
