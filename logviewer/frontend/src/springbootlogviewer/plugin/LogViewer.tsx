8e40e033ae547fcae51cbe7874a642ae41744b6d
Ahmet Erdinç Yılmaz <ahmeterdinc09@gmail.com>
2025-02-01 13:32:01 +0300

    add patternfly logview componenet

--- logviewer/frontend/src/springbootlogviewer/plugin/LogViewer.tsx
+++ logviewer/frontend/src/springbootlogviewer/plugin/LogViewer.tsx
@@ -1,17 +1,51 @@
-import { PageSection, Text, TextContent } from "@patternfly/react-core";
+import { PageSection } from "@patternfly/react-core";
+
+import {
+  LogViewer as PatternFlyLogViewer,
+  LogViewerSearch,
+} from "@patternfly/react-log-viewer";
+import {
+  Button,
+  Tooltip,
+  Toolbar,
+  ToolbarContent,
+  ToolbarGroup,
+  ToolbarItem,
+  ToolbarToggleGroup,
+} from "@patternfly/react-core";
+import OutlinedPlayCircleIcon from "@patternfly/react-icons/dist/esm/icons/outlined-play-circle-icon";
+import ExpandIcon from "@patternfly/react-icons/dist/esm/icons/expand-icon";
+import PauseIcon from "@patternfly/react-icons/dist/esm/icons/pause-icon";
+import PlayIcon from "@patternfly/react-icons/dist/esm/icons/play-icon";
+import EllipsisVIcon from "@patternfly/react-icons/dist/esm/icons/ellipsis-v-icon";
+import DownloadIcon from "@patternfly/react-icons/dist/esm/icons/download-icon";
+
 import React from "react";
 import { Client } from "@stomp/stompjs";
-import { useEffect, useState } from "react";
-import { TextArea } from "@patternfly/react-core";
+import { useEffect, useState, useRef } from "react";
 import { log } from "./globals";
 const SOCKET_URL = "ws://localhost:8080/hawtio-spring-boot-log-viewer";
 
-type Log = {
-  message: string;
-};
-
 export const LogViewer: React.FunctionComponent = () => {
-  const [logs, setLogs] = useState<Log[]>([]);
+  const [isPaused, setIsPaused] = useState<boolean>(false);
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+  const [currentItemCount, setCurrentItemCount] = useState<number>(0);
+  const [renderData, setRenderData] = useState<string>("");
+  const [buffer, setBuffer] = useState<string[]>([]);
+  const [linesBehind, setLinesBehind] = useState<number>(0);
+  const logViewerRef = useRef<any>();
+  const logViewerRefOuter = useRef<any>();
+
+  useEffect(() => {
+    if (!isPaused && buffer.length > 0) {
+      setCurrentItemCount(buffer.length);
+      setRenderData(buffer.join("\n"));
+    } else if (buffer.length !== currentItemCount) {
+      setLinesBehind(buffer.length - currentItemCount);
+    } else {
+      setLinesBehind(0);
+    }
+  }, [isPaused, buffer]);
 
   useEffect(() => {
     log.log("use effect executed");
@@ -21,7 +55,7 @@ export const LogViewer: React.FunctionComponent = () => {
         if (msg.body) {
           var jsonBody = JSON.parse(msg.body);
           if (jsonBody.message) {
-            setLogs((prevLogs) => [{ message: jsonBody.message }, ...prevLogs]);
+            setBuffer((prevLogs) => [jsonBody.message, ...prevLogs]);
             log.log("message received ", jsonBody.message);
           }
         }
@@ -44,14 +78,173 @@ export const LogViewer: React.FunctionComponent = () => {
     client.activate();
   }, []);
 
+  // Listening escape key on full screen mode.
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      const isFullscreen = document.fullscreenElement;
+      setIsFullScreen(!!isFullscreen);
+    };
+
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+    };
+  }, []);
+
+  const onExpandClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const element: Element = document.querySelector(
+      "#complex-toolbar-demo"
+    ) as Element;
+
+    //const element: Element = logViewerRefOuter.current;
+
+    if (!isFullScreen) {
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      }
+      setIsFullScreen(true);
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+      setIsFullScreen(false);
+    }
+  };
+
+  const onDownloadClick = () => {
+    const element = document.createElement("a");
+    const file = new Blob([buffer.join("\n")], {
+      type: "text/plain",
+      endings: "native",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = `log.txt`; //`${selectedDataSource}.txt`
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
+  const onScroll = ({
+    scrollDirection,
+    scrollOffset,
+    scrollOffsetToBottom,
+    //scrollUpdateWasRequested is false when the scroll event is cause by the user interaction in the browser, else it's true.
+    scrollUpdateWasRequested,
+  }: {
+    scrollDirection: "forward" | "backward";
+    scrollOffset: number;
+    scrollOffsetToBottom: number;
+    scrollUpdateWasRequested: boolean;
+  }) => {
+    log.log(
+      `scrollDirection = ${scrollDirection}, scrollOffsetToBottom  = ${scrollOffsetToBottom},  scrollUpdateWasRequested = ${scrollUpdateWasRequested}`
+    );
+    if (!scrollUpdateWasRequested && scrollDirection == "backward") {
+      if (scrollOffsetToBottom > 1) {
+        setIsPaused(true);
+      }
+    }
+    if (!scrollUpdateWasRequested && scrollDirection == "forward") {
+      if (scrollOffsetToBottom < 1) {
+        setIsPaused(false);
+      }
+    }
+  };
+
+  const ControlButton = () => (
+    <Button
+      variant="link"
+      onClick={() => {
+        setIsPaused(!isPaused);
+      }}
+      icon={isPaused ? <PlayIcon /> : <PauseIcon />}
+    >
+      {isPaused ? ` Resume Log` : ` Pause Log`}
+    </Button>
+  );
+
+  const leftAlignedToolbarGroup = (
+    <React.Fragment>
+      <ToolbarToggleGroup toggleIcon={<EllipsisVIcon />} breakpoint="md">
+        <ToolbarItem>
+          <LogViewerSearch
+            onFocus={(_e: any) => setIsPaused(true)}
+            placeholder="Search"
+            minSearchChars={4} // ?
+          />
+        </ToolbarItem>
+      </ToolbarToggleGroup>
+      <ToolbarItem>
+        <ControlButton />
+      </ToolbarItem>
+    </React.Fragment>
+  );
+
+  const rightAlignedToolbarGroup = (
+    <React.Fragment>
+      <ToolbarGroup variant="icon-button-group">
+        <ToolbarItem>
+          <Tooltip position="top" content={<div>Download</div>}>
+            <Button
+              onClick={onDownloadClick}
+              variant="plain"
+              aria-label="Download current logs"
+            >
+              <DownloadIcon />
+            </Button>
+          </Tooltip>
+        </ToolbarItem>
+        <ToolbarItem>
+          <Tooltip position="top" content={<div>Expand</div>}>
+            <Button
+              onClick={onExpandClick}
+              variant="plain"
+              aria-label="View log viewer in full screen"
+            >
+              <ExpandIcon />
+            </Button>
+          </Tooltip>
+        </ToolbarItem>
+      </ToolbarGroup>
+    </React.Fragment>
+  );
+
+  const FooterButton = () => {
+    const handleClick = (_e: React.MouseEvent<HTMLButtonElement>) => {
+      setIsPaused(false);
+    };
+    return (
+      <Button onClick={handleClick} isBlock>
+        <OutlinedPlayCircleIcon />
+        resume {linesBehind === 0 ? null : `and show ${linesBehind} lines`}
+      </Button>
+    );
+  };
   return (
-    <PageSection variant="light">
-      <TextArea
-        aria-label="invalid text area example"
-        resizeOrientation="both"
-        value={logs
-          .map((val) => val.message)
-          .reduce((acc, val) => val + "\n" + acc, "")}
+    <PageSection variant="default">
+      <PatternFlyLogViewer
+        data={renderData}
+        theme="light"
+        ref={logViewerRefOuter}
+        id="complex-toolbar-demo"
+        scrollToRow={currentItemCount}
+        innerRef={logViewerRef}
+        height={"100%"}
+        toolbar={
+          <Toolbar>
+            <ToolbarContent>
+              <ToolbarGroup align={{ default: "alignLeft" }}>
+                {leftAlignedToolbarGroup}
+              </ToolbarGroup>
+              <ToolbarGroup align={{ default: "alignRight" }}>
+                {rightAlignedToolbarGroup}
+              </ToolbarGroup>
+            </ToolbarContent>
+          </Toolbar>
+        }
+        overScanCount={10}
+        footer={isPaused && <FooterButton />}
+        onScroll={onScroll}
       />
     </PageSection>
   );
